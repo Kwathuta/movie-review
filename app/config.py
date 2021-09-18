9bbe0fbad32e50e776b25cad68edda06780fa5bb
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-18 22:16:14 +0300

    added db and functionality

--- app/config.py
+++ app/config.py
@@ -12,6 +12,18 @@ class Config:
     MOVIE_API_BASE_URL = "https://api.themoviedb.org/3/movie/{}?api_key={}"
     MOVIE_API_KEY = os.environ.get("MOVIE_API_KEY")
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://moringa:aljokela7247@localhost/watchlist"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
--- start.sh
+++
@@ -1,4 +0,0 @@
-export MOVIE_API_KEY="3ebfac97c85649554ce9c6320972c8ce"
-export SECRET_KEY="wtfkey1234"
-
-python3 manage.py server
